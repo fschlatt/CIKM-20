d954f896310579c509c0161aadb13fa7a99c8040
Ferdinand Schlatt <ferdinand.schlatt@informatik.uni-halle.de>
2021-09-24 10:36:39 +0200

    update scripts

--- scripts/extraction-common-crawl.sh
+++ scripts/extraction-common-crawl.sh
@@ -6,20 +6,20 @@ data1="/mnt/ceph/storage/data-in-progress/data-research/web-search/medical-quest
 data2="/mnt/ceph/storage/data-in-progress/data-research/web-search/medical-question-answering/causenet-data/external/stop-word-lists/enStopWordList.txt"
 data4="/mnt/ceph/storage/data-in-progress/data-research/web-search/medical-question-answering/causenet-data/external/ignore-html-lists/empty.txt"
 
-while read part
+while read segment
 do
-    echo $part
-    data0="\"/corpora/corpora-thirdparty/corpus-clueweb/12/*/ClueWeb12_"$part"/*/*.warc.gz\""
-    data3="/user/fschlatt/clueweb12/part"$part
+    echo $segment
+    data0="\"s3a://corpus-commoncrawl-main-2021-10/crawl-data/CC-MAIN-2021-10/segments/"$segment"/warc/*.warc.gz\""
+    data3="/user/fschlatt/common-crawl/"$segment
     hdfs dfs -rmdir --ignore-fail-on-non-empty $data3
 
     spark-submit \
-        --name "CauseNet ClueWeb12 Extraction Part "$part \
+        --name "CauseNet CommonCrawl Extraction "$segment \
         --master yarn \
         --deploy-mode cluster \
-        --num-executors 60 \
-        --executor-memory 16g \
-        --driver-memory 16g \
+        --num-executors 120 \
+        --executor-memory 8g \
+        --driver-memory 8g \
         --conf spark.executor.memoryOverhead=2g \
         --conf spark.dynamicAllocation.enabled=false \
         --conf spark.driver.memoryOverhead=2g \
@@ -27,4 +27,4 @@ do
         --conf spark.yarn.maxAppAttempts=1 \
         $jar $data0 $data1 $data2 $data3 $data4
 
-done < clueweb12-parts.txt
+done < common-crawl-segments.txt
--- scripts/extraction-wikipedia.sh
+++ scripts/extraction-wikipedia.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 ## variables
 jar="./java/extraction/target/extraction-1.0-SNAPSHOT-jar-with-dependencies.jar"
-data0="data/external/extraction-sources/wikipedia/enwiki-20181001-pages-articles.xml" 
-data1="data/bootstrapping/2-patterns" 
-data2="data/causality-graphs/extraction/wikipedia/wikipedia-extraction.tsv"
+data0="/mnt/ceph/storage/corpora/corpora-thirdparty/corpus-wikipedia/wikimedia-snapshots/enwiki-20210601/enwiki-20210601-pages-articles.xml.bz2" 
+data1="/mnt/ceph/storage/causenet-data/bootstrapping/2-patterns" 
+data2="/user/fschlatt/wikipedia"
 
 ## processing
-java -jar -Xmx150G -Xms150G $jar $data0 $data1 $data2
+spark-submit \
+    --name "CauseNet Wikipedia Extraction" \
+    --master yarn \
+    --deploy-mode cluster \
+    --num-executors 40 \
+    --executor-memory 12g \
+    --conf spark.yarn.submit.waitAppCompletion=false \
+    --conf spark.executor.memoryOverhead=12000 \
+    --conf spark.dynamicAllocation.enabled=false \
+    --conf spark.driver.memoryOverhead=2000 \
+    --conf spark.network.timeout=600 \
+    $jar $data0 $data1 $data2
