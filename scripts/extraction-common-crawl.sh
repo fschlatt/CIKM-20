e007f6221adb2e95c6575ddc1cfbe7a1f83d70ea
Ferdinand Schlatt <ferdinand.schlatt@informatik.uni-halle.de>
2021-12-01 11:44:42 +0100

    update commoncrawl script

--- scripts/extraction-common-crawl.sh
+++ scripts/extraction-common-crawl.sh
@@ -2,16 +2,17 @@
 ## variables
 jar="./java/extraction/target/extraction-1.0-SNAPSHOT-jar-with-dependencies.jar"
 
-data1="/mnt/ceph/storage/data-in-progress/data-research/web-search/medical-question-answering/causenet-data/bootstrapping/2-patterns"
-data2="/mnt/ceph/storage/data-in-progress/data-research/web-search/medical-question-answering/causenet-data/external/stop-word-lists/enStopWordList.txt"
-data4="/mnt/ceph/storage/data-in-progress/data-research/web-search/medical-question-answering/causenet-data/external/ignore-html-lists/empty.txt"
+data1="/mnt/ceph/storage/data-in-progress/data-research/web-search/health-question-answering/causenet-data/bootstrapping/2-patterns"
+data2="/mnt/ceph/storage/data-in-progress/data-research/web-search/health-question-answering/causenet-data/external/stop-word-lists/enStopWordList.txt"
+data4="/mnt/ceph/storage/data-in-progress/data-research/web-search/health-question-answering/causenet-data/external/ignore-html-lists/empty.txt"
+data5="/mnt/ceph/storage/data-in-progress/data-research/web-search/health-question-answering/.s3cfg"
 
 while read segment
 do
     echo $segment
     data0="\"s3a://corpus-commoncrawl-main-2021-10/crawl-data/CC-MAIN-2021-10/segments/"$segment"/warc/*.warc.gz\""
     data3="/user/fschlatt/common-crawl/"$segment
-    hdfs dfs -rmdir --ignore-fail-on-non-empty $data3
+    hdfs dfs -rm -r $data3
 
     spark-submit \
         --name "CauseNet CommonCrawl Extraction "$segment \
@@ -25,6 +26,6 @@ do
         --conf spark.driver.memoryOverhead=2g \
         --conf spark.network.timeout=600 \
         --conf spark.yarn.maxAppAttempts=1 \
-        $jar $data0 $data1 $data2 $data3 $data4
+        $jar $data0 $data1 $data2 $data3 $data4 $data5
 
 done < common-crawl-segments.txt
